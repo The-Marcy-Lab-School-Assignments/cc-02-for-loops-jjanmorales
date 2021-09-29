f51c56235b71d93cfced157f725cc5947bdeda12
Jan Morales <moralesjan40@gmail.com>
2021-09-29 14:49:29 +0000

    Completed all problems

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 //1
 function countToTen(){
-  
+  for(let i = 0; i < 11; i++){
+      console.log(i)
+  }
 }
+// countToTen();
+
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+  for(let i = 1; i <= num; i++){
+      console.log(i)
+  }
 }
+// countFromOne(18);
+
 
 //3
-function countEveryEven(){
-  
+function countEveryEven(num){
+  for(let i = 1; i <= num; i++){
+      if( i % 2 === 0){
+          console.log(i)
+      }
+  }
 }
+// countEveryEven(48);
 
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for(let i = 1; i <= num; i+=2){
+      console.log(i)
+  }
 }
+// countEveryOdd(19);
 
 //5 
-function countEvens(){
-  
+function countEvens(nums){
+  let count = 0
+  for(let i = 0; i < nums.length; i++){
+    if( nums[i] % 2 === 0){
+      count+=1
+    }
+  }
+  return count
 }
-
+// console.log(countEvens([1,14,2,6,8]))
